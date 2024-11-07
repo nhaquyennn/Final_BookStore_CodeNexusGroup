f14c1aacf8b5a8ad26e060e1484dfb4c2156e488
Hồ Trường Minh Phú <117896798+phu213000@users.noreply.github.com>
2024-11-07 23:33:18 +0700

    upload lan dau

--- ADMIN/statistics.php
+++ ADMIN/statistics.php
@@ -23,9 +23,49 @@
                 <!--End topbar header-->
 
                 <!--Start main content-->
-
+        
                 <!-- CODE Ở ĐÂY -->
-
+                <div class="card mt-3">
+                    <div class="card-content">
+                        <div class="row row-group m-0">
+                            <div class="col-12 col
+                            -lg-6 col-xl-4 border-light">
+                                <div class="card-body">
+                                    <h5 class="text-white
+                                    mb-0">22 <span class="float-right"><i class="fa fa-shopping-cart"></i></span>       
+                                    </h5>
+                                    <div class="progress my-3" style="height:3px;">
+                                        <div class="progress-bar" style="width:55%"></div>
+                                    </div>
+                                    <p class="mb-0 text-white small-font">Tổng đơn hàng hôm nay</p>
+                                </div>
+                            </div>
+                            <div class="col-12 col -lg-6 col-xl-4 border-light">
+                                <div class="card-body">
+                                    <h5 class="text-white
+                                    mb-0">100.000.000 <span class="float-right"><i class="fa fa-usd"></i></span>
+                                    </h5>
+                                    <div class="progress my-3" style="height:3px;">
+                                        <div class="progress-bar" style="width:55%"></div>
+                                    </div>
+                                    <p class="mb-0 text-white small-font">Tổng doanh thu hôm nay</p>
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-6 col-xl-4 border-light">
+                                <div class="card-body">
+                                    <h5 class="text-white
+                                    mb-0">3 <span class="float-right"><i class="zmdi zmdi-assignment"></i></span>
+                                    </h5>
+                                    <div class="progress my-3" style="height:3px;">
+                                        <div class="progress-bar" style="width:55%"></div>
+                                    </div>
+                                    <p class="mb-0 text-white small-font">Yêu cầu</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
                 <!--End main content-->
 
             </div>
