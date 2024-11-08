5afd483a40e29c08da068b90e026b3355dc10e61
Hồ Trường Minh Phú <117896798+phu213000@users.noreply.github.com>
2024-11-08 17:17:52 +0700

    sua footer, header,left side-bar va xoa stattis.php thanh report

--- ADMIN/report/month_statistics.php
+++ ADMIN/report/month_statistics.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <?php require_once "layout/header.php"; ?>
+    <?php require_once "../layout/header.php"; ?>
 </head>
 
 <body class="bg-theme bg-theme2">
@@ -13,12 +13,12 @@
             <div id="wrapper">
 
                 <!--Start sidebar-wrapper-->
-                <?php require_once "layout/left_sidebar.php"; ?>
+                <?php require_once "../layout/left_sidebar.php"; ?>
                 <!--End sidebar-wrapper-->
 
                 <!--Start topbar header-->
                 <header class="topbar-nav">
-                    <?php require_once "layout/topbar.php"; ?>
+                    <?php require_once "../layout/topbar.php"; ?>
                 </header>
                 <!--End topbar header-->
 
@@ -62,11 +62,11 @@
             <!-- End wrapper-->
 
             <!--Start right sidebar-->
-            <?php require_once "layout/right_sidebar.php"; ?>
+            <?php require_once "../layout/right_sidebar.php"; ?>
             <!--End right sidebar-->
 
             <!--Start footer-->
-            <?php require_once "layout/script.php"; ?>
+            <?php require_once "../layout/script.php"; ?>
             <!--End footer-->
         </div>
     </div>
