f0840a915be2c34a0c1585217108c17a19d5e445
Hồ Trường Minh Phú <117896798+phu213000@users.noreply.github.com>
2024-12-03 23:43:43 +0700

    thêm chú thích

--- ADMIN/config/config.php
+++ ADMIN/config/config.php
@@ -2,3 +2,4 @@
 #khai báo điều hướng report 
 define("BASE_URL", "http://localhost:84/bookstore/ADMIN/");
 define("BASE_PATH", __DIR__ . "/../");
+# trong file này là file cấu hình về  bạn chỉ cần đổi tên bookstore thành tên app của bạn là dc ngay trên url 
